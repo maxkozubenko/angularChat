ce5e0be6c9389375f22aa32d3f58e86c74ca2ceb
Maksym Kozubenko <Maksym_Kozubenko@epam.com>
2022-08-16 12:35:00 +0300

    add init angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedComponentsModule} from "./shared/shared-components.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatMenuComponent } from './components/chat-menu/chat-menu.component';
+import { ChatMessagesComponent } from './components/chat-messages/chat-messages.component';
+import { ChatContactsComponent } from './components/chat-contacts/chat-contacts.component';
+import { ChatDetailsComponent } from './components/chat-details/chat-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    ChatMenuComponent,
+    ChatMessagesComponent,
+    ChatContactsComponent,
+    ChatDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedComponentsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
